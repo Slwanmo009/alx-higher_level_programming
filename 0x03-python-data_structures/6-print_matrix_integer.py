22499b585f75b0c6bb7dc9ef759d12f9fbf68421
Slwanmo009 <slwanmohammed9@gmail.com>
2024-03-18 10:19:14 -0700

    coz

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for sub in matrix:
         if len(sub) == 0:
             print()
-            for i in range(len(sub)):
-                print("{:d}".format(sun[i]),
+        for i in range(len(sub)):
+            print("{:d}".format(sun[i]),
                         end="\n" if i is len(sub) - 1 else " ")
