b47baac1bd81bf6e0c2f4ebb0adce99df5b8542e
slwanmo009 <slwanmohammed9@gmail.com>
2024-06-19 10:41:30 -0700

    12

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * This script defines an empty class Rectangle.
+ */
+
+class Rectangle {
+  // Empty class definition
+}
+
+// Export the Rectangle class to make it available for import in other modules
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * This script defines a class Rectangle that takes two arguments: w and h.
+ * It initializes the instance attributes width and height with the values of w and h.
+ */
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// Export the Rectangle class to make it available for import in other modules
+module.exports = Rectangle;
