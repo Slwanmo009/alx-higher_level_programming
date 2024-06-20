cc20e1332a3989e5caebec2ea125990e4f907acf
slwanmo009 <slwanmohammed9@gmail.com>
2024-06-19 17:02:19 -0700

    hp

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-/**
- * This script defines a class Rectangle that takes two arguments: w and h.
- * It initializes the instance attributes width and height with the values of w and h.
- */
 
 class Rectangle {
 	constructor(w, h) {
@@ -10,5 +6,4 @@ class Rectangle {
 		this.height = h;
 	}
 }
-// Export the Rectangle class to make it available for import in other modules
 module.exports = Rectangle;
